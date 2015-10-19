d4c8581dc4d317663b494de4b15fc3f6c0409838
Wolfy Meier <wolfy@wlfy.it>
2015-10-19 15:00:21 -0700

    added "vocalDebug" variable and mode

--- src/main/java/haus/pup/Karotz.java
+++ src/main/java/haus/pup/Karotz.java
@@ -2,23 +2,38 @@ package haus.pup;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CFactory;
 import haus.pup.karotzhw.*;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
+
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Karotz {
   public static void main(String args[]) {
-    String dbURI = "jdbc:h2:" + new File("karotz").getAbsolutePath();
+    // Init Logging
     Logger logger = LoggerFactory.getLogger("Karotz");
 
-    // Conncet to DB
-    logger.info("Connecting to DB at :" + dbURI);
-    Connection db = null;
+    // Enable spoken debugging
+    Boolean vocalDebug = false;
 
+    if (Objects.equals(System.getProperty("vocaldebug"), "true")) {
+      vocalDebug = true;
+    }
+    logger.info("Vocal Debugger: " + vocalDebug.toString());
+
+    // Init DB
+    String dbURI = "jdbc:h2:" + new File("karotz").getAbsolutePath();
+    logger.info("Connecting to DB at :" + dbURI);
+
+    Connection db = null;
     try {
       db = DriverManager.getConnection(dbURI);
     } catch (SQLException e) {
@@ -31,12 +46,43 @@ public class Karotz {
       logger.error("Failed to make connection!");
     }
 
-    //final GpioController gpio = GpioFactory.getInstance();
-
+    // Init Speech
     Speech speech = new Speech();
+    if (vocalDebug) {
+      speech.say("Speech Initialized");
+    }
+
+    // Init RasPi Hardware
+    //GpioController gpio = GpioFactory.getInstance();
+
+    /*I2CBus i2cbus = null;
+    try {
+      i2cbus = I2CFactory.getInstance(1);
+    } catch (IOException e) {
+      speech.say("I could not open i squared c bus 1");
+      logger.error("Could not open I2C bus 1");
+      e.printStackTrace();
+    }*/
+
     //Ears ears = new Ears(gpio);
 
-    speech.say("Well I guess that makes you a couple of naughty stewardeses.");
+    greet(speech);
+  }
+
+  private static void greet(Speech s) {
+    DateTime dt = new DateTime();
+
+    if (dt.getHourOfDay() < 12 ) {
+      s.say("Good Morning");
+    }
+    else if (dt.getHourOfDay() > 11 && dt.getHourOfDay() < 17 ) {
+      s.say("Good Afternoon");
+    }
+    else {
+      s.say("Good Evening");
+    }
+
+    s.say("I am ready to work.");
 
   }
 }
--- src/main/java/haus/pup/karotzhw/Speech.java
+++ src/main/java/haus/pup/karotzhw/Speech.java
@@ -60,8 +60,6 @@ public class Speech {
         }
       }
     }
-
-    say("Speech Initialized");
   }
 
   /**
